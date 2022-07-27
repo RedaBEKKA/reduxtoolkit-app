3ac0051b209ec8f354c347ddd738efa608dddb9d
Reda BEKKA <reda.bekka@gmail.com>
2022-07-27 15:57:14 +0200

    feat(Users):add users redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { addUser } from "./features/Users";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [username, setUserName] = useState("");
+  const userList = useSelector((state) => state.users.value);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="addUser">
+        <input
+          type="text"
+          placeholder="Votre Nom..."
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Votre pseudo..."
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <button
+          onClick={() =>
+            dispatch(
+              addUser({
+                id: userList[userList.length - 1].id + 1,
+                name,
+                username,
+              })
+            )
+          }
         >
-          Learn React
-        </a>
-      </header>
+          Ajouter un utilisateur
+        </button>
+      </div>
+      <div className="displayUsers">
+        {userList.map((eachUser) => {
+          return (
+            <>
+              <h1>{eachUser.name}</h1>
+              <h1>{eachUser.username}</h1>
+            </>
+          );
+        })}
+      </div>
     </div>
   );
 }
